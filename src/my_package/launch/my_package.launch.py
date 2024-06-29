284d467ece68f056c583590ea59177f2b185fe56
cramke <carsten.ramke@gmail.com>
2024-06-29 11:33:15 +0200

    [my_package] Simplify launch script

--- src/my_package/launch/my_package.launch.py
+++ src/my_package/launch/my_package.launch.py
@@ -4,19 +4,15 @@ import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    # Specify your package name and node executable
-    package_name = "my_package"
-    node_executable = "my_node"
-    
+def generate_launch_description():    
     # Create a ROS 2 launch description object
     ld = LaunchDescription()
 
     # Add the ROS 2 node to the launch description
     node = Node(
-        package=package_name,
-        executable=node_executable,
-        name=node_executable,
+        package="my_package",
+        executable="my_node",
+        name="my_node",
         output='screen'
     )
     ld.add_action(node)
